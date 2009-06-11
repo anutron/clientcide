d32d1d2a548111b6a1a281aec89919dcd88555bd
Aaron Newton <git@clientcide.com>
2009-06-11 16:20:16 -0700

    updating config management

--- Tests/config.js
+++ Tests/config.js
@@ -1,23 +1,24 @@
-UnitTester.site = 'Clientcide';
-UnitTester.title = 'Unit Tests';
-
-window.addEvent('load', function(){
-	var sources = {
-		mootoolsCore: '../../core',
-		mootoolsMore: '../../more',
-		clientcide: '..'
-	};
-	if (window.location.href.contains("http://www.clientcide.com/tests")) {
-		sources = {
-			mootoolsCore: '/cnet.gf/Mootools/mootools-core',
-			mootoolsMore: '/cnet.gf/Mootools/mootools-more',
-			clentcide: '/cnet.gf/svn'
+var UnitTester = {
+	site: 'Clientcide',
+	title: 'Unit Tests',
+	path: '../UnitTester/',
+	ready: function(){
+		var sources = {
+			mootoolsCore: '../../core',
+			mootoolsMore: '../../more',
+			clientcide: '..'
 		};
+		if (window.location.href.contains("http://www.clientcide.com/tests")) {
+			sources = {
+				mootoolsCore: '/cnet.gf/Mootools/mootools-core',
+				mootoolsMore: '/cnet.gf/Mootools/mootools-more',
+				clentcide: '/cnet.gf/svn'
+			};
+		}
+		new UnitTester(sources, {
+			Clientcide: 'UserTests/'
+		}, {
+			autoplay: true
+		});
 	}
-	
-	new UnitTester(sources, {
-		Clientcide: 'UserTests/'
-	}, {
-		autoplay: true
-	});
-});
+};
--- Tests/path.js
+++
@@ -1,1 +0,0 @@
-var path = '../UnitTester/';

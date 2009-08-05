747e53c1ab92a6a3c2055d53bf88fac426529cb1
Aaron Newton <git@clientcide.com>
2009-08-05 11:08:09 -0700

    adding new methods for HtmlTable to support thead and tfoot content.

--- Source/Layout/HtmlTable.js
+++ Source/Layout/HtmlTable.js
@@ -14,26 +14,42 @@ var HtmlTable = new Class({
 			cellspacing: 0,
 			border: 0
 		},
-		rows: []
+		rows: [],
+		headers: []
 	},
 	initialize: function(options) {
 		this.setOptions(options);
-		this.table = new Element('table').setProperties(this.options.properties);
+		this.table = new Element('table', this.options.properties);
 		this.table.store('HtmlTable', this);
+		if (this.options.headers.length) {
+			this.setHeaders(this.options.headers);
+		}
 		this.tbody = new Element('tbody').inject(this.table);
 		this.options.rows.each(this.push.bind(this));
-		["adopt", "inject", "wraps", "grab", "replaces", "empty", "dispose"].each(function(method){
+		["adopt", "inject", "wraps", "grab", "replaces", "dispose"].each(function(method){
 				this[method] = this.table[method].bind(this.table);
 		}, this);
+	},
+	empty: function(){
+		this.tbody.empty();
 	},
 	toElement: function(){
 		return this.table;
 	},
-	push: function(row) {
-		var tr = new Element('tr').inject(this.tbody);
+	setHeaders: function(headers){
+		if (!this.thead) this.thead = new Element('thead').inject(this.table, 'top');
+		else this.thead.empty();
+		this.push(headers, this.thead, 'th');
+	},
+	setFooters: function(footers) {
+		if (!this.tfoot) this.tfoot = new Element('tfoot').inject(this.table, 'top');
+		this.push(footers, this.tfoot);
+	},
+	push: function(row, target, tag) {
+		var tr = new Element('tr').inject(target || this.tbody);
 		var tds = row.map(function (tdata) {
 			tdata = tdata || '';
-			var td = new Element('td').inject(tr);
+			var td = new Element(tag || 'td').inject(tr);
 			if (tdata.properties) td.setProperties(tdata.properties);
 			function setContent(content){
 				if (document.id(content)) td.adopt(document.id(content));

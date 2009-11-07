9c9df10632ac3400f50a394c50444977bd09f537
Aaron Newton <git@clientcide.com>
2009-11-07 14:15:45 -0800

    getting tag maker working again.

--- Source/Forms/TagMaker.js
+++ Source/Forms/TagMaker.js
@@ -99,75 +99,75 @@ var TagMaker = new Class({
 		var opt = this.options; //save some bytes
 		if (!this.form) { //if the body hasn't been created, create it
 			this.form = new Element('form');
-				var table = new HtmlTable({properties: {'class':'trinket'}});
-				this.getKeys(opt.output).each(function(inputKey) {
-					var input;
-					if (this.options.selectLists[inputKey]){
-						input = new Element('select').setProperties({
-							name: inputKey.replace(' ', '-', 'g')
-						}).addEvent('change', this.createOutput.bind(this));
-						this.options.selectLists[inputKey].each(function(opt){
-							var option = new Element('option').inject(input);
-							if (opt.selected) option.set('selected', true);
-							option.set('value', opt.value);
-							option.set('text', opt.key);
-						}, this);
-						table.push([inputKey, input]);
-					} else {
-						input = new Element('input', {
-							type: 'text',
-							name: inputKey.replace(/ /g, '-'),
-							title: inputKey+'::'+opt.help[inputKey],
-							'class': 'text tip ' + ((opt['class'])?opt['class'][inputKey]||'':''),
-							events: {
-								keyup: this.createOutput.bind(this),
-								focus: function(){this.select();},
-								change: this.createOutput.bind(this)
-							}
-						});
-						if (opt.picklets[inputKey]) {
-							var a = new Element('a').addClass('button').set('html', 'choose');
-							var div = new Element('div').adopt(input.setStyle('width',160)).adopt(a);
-							var picklets = ($type(opt.picklets[inputKey]) == "array")?opt.picklets[inputKey]:[opt.picklets[inputKey]];
-							new ProductPicker(input, picklets, {
-								showOnFocus: false, 
-								additionalShowLinks: [a],
-								onPick: function(input, data, picker){
-									try {
-										var ltInput = this.getInnerTextInput();
-										if (ltInput && !ltInput.get('value')) {
-											try {
-												ltInput.set('value', picker.currentPicklet.options.listItemName(data));
-											}catch (e){dbug.log('set value error: ', e);}
-										}
-										var val = input.value;
-										if (inputKey == "Full Path" && val.test(/^http:/))
-												input.set('value', val.substring(val.indexOf('/', 7), val.length));
-										this.createOutput();
-									} catch(e){dbug.log(e);}
-								}.bind(this)
-							});
-							table.push([inputKey, div]);
-						} else table.push([inputKey, input]);
-					}
-					//[{content: <content>, properties: {colspan: 2, rowspan: 3, 'class': "cssClass", style: "border: 1px solid blue"}]
-					if (this.options.example[inputKey]) 
-						table.push([{content: 'eg. '+this.options.example[inputKey], properties: {colspan: 2, 'class': 'example'}}]);
-				}, this);
-				this.resultInput = new Element('input', {
+			var table = new HtmlTable({properties: {'class':'trinket'}});
+			this.getKeys(opt.output).each(function(inputKey) {
+				var input;
+				if (this.options.selectLists[inputKey]){
+					input = new Element('select').setProperties({
+						name: inputKey.replace(' ', '-', 'g')
+					}).addEvent('change', this.createOutput.bind(this));
+					this.options.selectLists[inputKey].each(function(opt){
+						var option = new Element('option').inject(input);
+						if (opt.selected) option.set('selected', true);
+						option.set('value', opt.value);
+						option.set('text', opt.key);
+					}, this);
+					table.push([inputKey, input]);
+				} else {
+					input = new Element('input', {
 						type: 'text',
-						title: 'HTML::This is the resulting tag html.',
-						'class': 'text result tip'
-					}).addEvent('focus', function(){this.select();});
-				table.push(['HTML', this.resultInput]).tr;
-
-			this.form = table.table;
+						name: inputKey.replace(/ /g, '-'),
+						title: inputKey+'::'+opt.help[inputKey],
+						'class': 'text tip ' + ((opt['class'])?opt['class'][inputKey]||'':''),
+						events: {
+							keyup: this.createOutput.bind(this),
+							focus: function(){this.select();},
+							change: this.createOutput.bind(this)
+						}
+					});
+					if (opt.picklets[inputKey]) {
+						var a = new Element('a').addClass('button').set('html', 'choose');
+						var div = new Element('div').adopt(input.setStyle('width',160)).adopt(a);
+						var picklets = ($type(opt.picklets[inputKey]) == "array")?opt.picklets[inputKey]:[opt.picklets[inputKey]];
+						new ProductPicker(input, picklets, {
+							showOnFocus: false, 
+							additionalShowLinks: [a],
+							onPick: function(input, data, picker){
+								try {
+									var ltInput = this.getInnerTextInput();
+									if (ltInput && !ltInput.get('value')) {
+										try {
+											ltInput.set('value', picker.currentPicklet.options.listItemName(data));
+										}catch (e){dbug.log('set value error: ', e);}
+									}
+									var val = input.value;
+									if (inputKey == "Full Path" && val.test(/^http:/))
+											input.set('value', val.substring(val.indexOf('/', 7), val.length));
+									this.createOutput();
+								} catch(e){dbug.log(e);}
+							}.bind(this)
+						});
+						table.push([inputKey, div]);
+					} else table.push([inputKey, input]);
+				}
+				//[{content: <content>, properties: {colspan: 2, rowspan: 3, 'class': "cssClass", style: "border: 1px solid blue"}]
+				if (this.options.example[inputKey]) 
+					table.push([{content: 'eg. '+this.options.example[inputKey], properties: {colspan: 2, 'class': 'example'}}]);
+			}, this);
+			this.resultInput = new Element('input', {
+					type: 'text',
+					title: 'HTML::This is the resulting tag html.',
+					'class': 'text result tip'
+				}).addEvent('focus', function(){this.select();});
+			table.push(['HTML', this.resultInput]).tr;
+			this.form.adopt($(table));
 			this.body = new Element('div', {
 				styles: {
 					overflow:'auto',
 					maxHeight: this.options.maxHeight
 				}
 			}).adopt(this.form);
+
 			this.validator = new Form.Validator.Inline(this.form);
 			this.validator.insertAdvice = function(advice, field){
 				var p = document.id(field.parentNode);
--- Tests/UserTests/Forms/TagMaker.all.js
+++ Tests/UserTests/Forms/TagMaker.all.js
@@ -5,9 +5,9 @@
 			description: "Prompts the user to fill in data for an image tag.",
 			verify: "Were you prompted to fill in the image info? Did it produce the proper image tag based on your selections?",
 			before: function(){
+				Clientcide.setAssetLocation('../Assets');
 				new TagMaker.image().prompt($('tagMakerTest'));
-			},
-			body: ""
+			}
 		}
 	]
 }

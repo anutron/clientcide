ec9da10b7e580cf9ea241999c9e94fd45819c076
Aaron Newton <git@clientcide.com>
2009-12-11 14:37:59 -0800

    adding german simple editor translation

---
+++ Docs/Localization/SimpleEditor.German.md
@@ -0,0 +1,16 @@
+Class: SimpleEditor {#SimpleEditor}
+=====================================
+
+Contains the messages for [SimpleEditor][] in German.
+
+### Authors
+
+* Wassilios Meletiadis
+
+### Usage
+
+Simply change the language property to *de-DE*.
+
+	MooTools.lang.setLanguage("de-DE");
+
+[SimpleEditor]: http://www.clientcide.com/docs/Forms/SimpleEditor
---
+++ Source/Localization/SimpleEditor.German.js
@@ -0,0 +1,27 @@
+/*
+---
+
+script: SimpleEditor.German.js
+
+description: SimpleEditor messages in German.
+
+license: MIT-Style License
+
+authors: [Wassilios Meletiadis]
+
+requires:
+- more:1.2.4.2/MooTools.Lang
+
+
+provides:
+- SimpleEditor.German
+
+...
+*/
+MooTools.lang.set('de-DE', 'SimpleEditor', {
+	woops:'Upps',
+	linkURL:'Der URL fÃ¼r den Link',
+	linkText:'Der Link-Text',
+	imgURL:'Der URL fÃ¼r die Bilddatei',
+	imgAlt:'Der Titel (alt) fÃ¼r die Bilddatei'
+});

9182f31d29b1efc6b444b4f3e44fda896926e260
Aaron Newton <git@clientcide.com>
2011-05-01 17:17:59 -0700

    adding view source yml data

--- Specs/clientcide/Behaviors/Behavior.Tips.js
+++ Specs/clientcide/Behaviors/Behavior.Tips.js
@@ -28,7 +28,7 @@ provides: [Behavior.Tips.Tests]
 		specs: false
 	});
 
-	var help = '<p class="jframe-inline" data-filters="HelpTip">I\'m some help text that gets turned into a help tip</p>';
+	var help = '<p data-filters="HelpTip">I\'m some help text that gets turned into a help tip</p>';
 	Behavior.addFilterTest({
 		filterName: 'HelpTip',
 		desc: 'Creates an instance of Tips.Pointy (help)',
@@ -47,7 +47,7 @@ provides: [Behavior.Tips.Tests]
 		specs: false
 	});
 
-	var info = '<p class="jframe-inline" data-filters="InfoTip">I\'m some help text that gets turned into a help tip</p>';
+	var info = '<p data-filters="InfoTip">I\'m some help text that gets turned into a help tip</p>';
 	Behavior.addFilterTest({
 		filterName: 'InfoTip',
 		desc: 'Creates an instance of Tips.Pointy (info)',

4751d4cf2f2a0c35d0e1a4e0ce98bb3810b31319
Aaron Newton <git@clientcide.com>
2011-10-17 20:57:49 -0700

    StickyWin.Fx doesn't work in IE and I don't feel like debugging it. IE users get no fading! Deal with it!

--- Source/UI/StickyWin.Fx.js
+++ Source/UI/StickyWin.Fx.js
@@ -13,54 +13,55 @@ provides: StickyWin.Fx
 
 ...
 */
-
-StickyWin = Class.refactor(StickyWin, {
-	options: {
-		//fadeTransition: 'sine:in:out',
-		fade: true,
-		fadeDuration: 150
-	},
-	hideWin: function(){
-		if (this.options.fade) this.fade(0);
-		else this.previous();
-	},
-	showWin: function(){
-		if (this.options.fade) this.fade(1);
-		else this.previous();
-	},
-	hide: function(){
-		this.previous(this.options.fade);
-	},
-	show: function(){
-		this.previous(this.options.fade);
-	},
-	fade: function(to){
-		if (!this.fadeFx) {
-			this.win.setStyles({
-				opacity: 0,
-				display: 'block'
-			});
-			var opts = {
-				property: 'opacity',
-				duration: this.options.fadeDuration
-			};
-			if (this.options.fadeTransition) opts.transition = this.options.fadeTransition;
-			this.fadeFx = new Fx.Tween(this.win, opts);
-		}
-		if (to > 0) {
-			this.win.setStyle('display','block');
-			this.position();
-		}
-		this.fadeFx.clearChain();
-		this.fadeFx.start(to).chain(function (){
-			if (to == 0) {
-				this.win.setStyle('display', 'none');
-				this.fireEvent('onClose');
-			} else {
-				this.fireEvent('onDisplay');
+if (!Browser.Engine.trident){
+	StickyWin = Class.refactor(StickyWin, {
+		options: {
+			//fadeTransition: 'sine:in:out',
+			fade: true,
+			fadeDuration: 150
+		},
+		hideWin: function(){
+			if (this.options.fade) this.fade(0);
+			else this.previous();
+		},
+		showWin: function(){
+			if (this.options.fade) this.fade(1);
+			else this.previous();
+		},
+		hide: function(){
+			this.previous(this.options.fade);
+		},
+		show: function(){
+			this.previous(this.options.fade);
+		},
+		fade: function(to){
+			if (!this.fadeFx) {
+				this.win.setStyles({
+					opacity: 0,
+					display: 'block'
+				});
+				var opts = {
+					property: 'opacity',
+					duration: this.options.fadeDuration
+				};
+				if (this.options.fadeTransition) opts.transition = this.options.fadeTransition;
+				this.fadeFx = new Fx.Tween(this.win, opts);
 			}
-		}.bind(this));
-		return this;
-	}
-});
+			if (to > 0) {
+				this.win.setStyle('display','block');
+				this.position();
+			}
+			this.fadeFx.clearChain();
+			this.fadeFx.start(to).chain(function (){
+				if (to == 0) {
+					this.win.setStyle('display', 'none');
+					this.fireEvent('onClose');
+				} else {
+					this.fireEvent('onDisplay');
+				}
+			}.bind(this));
+			return this;
+		}
+	});
+}
 StickyWin.Fx = StickyWin;
